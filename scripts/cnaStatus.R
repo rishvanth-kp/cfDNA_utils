e92812f5ee431a718a8841adae15c68e34625444
rishvanth-kp <rishvanth@gmail.com>
2020-08-27 15:29:20 -0700

    added code determine CNA status for specified regions

--- scripts/cnaStatus.R
+++ scripts/cnaStatus.R
@@ -15,37 +15,61 @@
 # General Public License for more details.
 
 main <- function() {
-  
+
   args = commandArgs(trailingOnly=TRUE)
   if (length(args) != 5) {
     stop("cnaStatus.R <cna.csv> <chrom-regions.csv> <gain-thresold>
-          <loss-threshold> <outfile.csv>", call.=FALSE)
+          <loss-threshold> <outfile-prefix>", call.=FALSE)
   }
 
-  # col 1-3 (header): chr (chrom), chr pos (chrompos), 
+  # col 1-3 (header): chr (chrom), chr pos (chrompos),
   #   absolute chr pos (abspos).
   # col 4- (header): one column per sample (sample name)
   # rows: segmented bin values, one row per bin
   cna <- read.csv(args[1], header = TRUE)
-  # col 1-4 (no header): chr, chr start, chr end, region name
-  # one row per region. 
+  # col 1-4 (no header): region name, chr, chr start, chr end
+  # one row per region. chr is encoded as 1-24 to keep it compatible
+  # with the segmented csv file from the CNV app
   regions <- read.csv(args[2], header = FALSE)
-  names(regions) <- c("chr", "chr.start", "chr.end", "name")
-  # gain and loss threshold 
+  names(regions) <- c("name", "chrom", "chrom.start", "chrom.end")
+  # gain and loss threshold
   gain.th <- as.numeric(args[3])
   loss.th <- as.numeric(args[4])
-  # csv file to output gain/loss status for each region
-  outfile <- args[5]
+  # outfile prefix
+  outfile.prefix <- args[5]
 
-  print(dim(cna))
-  print(gain.th)
-  print(loss.th)  
   n.samples <- ncol(cna) - 3
+  sample.names <- names(cna[,4:ncol(cna)])
 
+  out.median <- c()
+  out.status <- c()
   for (i in 1 : nrow(regions)) {
-    print(regions[i,])
+    cna.in.region <- cna[which(cna$chrom == regions[i,]$chrom &
+                               cna$chrompos >= regions[i,]$chrom.start &
+                               cna$chrompos < regions[i,]$chrom.end),
+                               4:ncol(cna)]
+    region.median <- sapply(cna.in.region, median)
+
+    region.status <- rep("neutral", times=n.samples)
+    region.status[region.median >= gain.th] <- "gain"
+    region.status[region.median <= loss.th] <- "loss"
+
+    out.median <- rbind(out.median, region.median)
+    out.status <- rbind(out.status, region.status)
+
   }
 
+  out.status <- as.data.frame(out.status, row.names = FALSE)
+  names(out.status) <- sample.names
+  out.status <- cbind(regions, out.status)
+  write.csv(out.status, quote = FALSE, row.names = FALSE,
+            file = sprintf("%s_status.csv", outfile.prefix))
+
+  out.median <- as.data.frame(out.median, row.names = FALSE)
+  names(out.median) <- sample.names
+  out.median <- cbind(regions, out.median)
+  write.csv(out.median, quote = FALSE, row.names = FALSE,
+            file = sprintf("%s_median.csv", outfile.prefix))
 }
 
 main()
